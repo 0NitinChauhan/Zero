2f7257faf2206980f5212e5d7f580bd685bb0ac6
Nitin Chauhan <nitin@Neptune.local>
2019-10-04 13:23:24 -0400

    leap year bug

--- src/LogLib.py
+++ src/LogLib.py
@@ -141,8 +141,12 @@ class LogLib:
     def is_leap_year(year):
         if year % 4 != 0:
             return False
-        elif year % 100 != 0:
-            return False
-        elif year % 400 != 0:
+        elif year % 400 == 0:
+            return True
+        elif year % 100 == 0:
             return False
         return True
+
+
+if __name__ == "__main__":
+    print(LogLib.is_leap_year(2016))
